c6c26c7b146961de537accf8d0bb0b2ad46417ea
Hiew Ngwen <134056139+itsmengwh08@users.noreply.github.com>
2025-08-02 19:50:23 +0700

    Create jammy.sh

---
+++ jammy.sh
@@ -0,0 +1,10 @@
+echo "Preparing"
+sudo dpkg --add-architecture i386
+echo "Adding key of official Wine repo"
+sudo mkdir -pm755 /etc/apt/keyrings
+wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+echo "Updating packages"
+sudo apt update -y && sudo apt upgrade -y
+echo "Installing Wine"
+sudo apt install --install-recommends winehq-stable -y
